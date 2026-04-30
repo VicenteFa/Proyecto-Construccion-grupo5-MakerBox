c853972b526d89b59bce350bec089400139a3fe1
Miyata-Dev <hklosteverlong@gmail.com>
2026-04-30 10:33:38 -0400

    fix(backend): Actualizar el backend para poder desplegar la aplicación

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+const API_PREFIX = 'api';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: [`/${API_PREFIX}/(.*)`],
+    }),
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })
