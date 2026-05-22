7a8a8b2a0924a1a75b4389bb7a61845c81c61474
Miyata-Dev <hklosteverlong@gmail.com>
2026-05-22 17:34:39 -0400

    feat(prisma): Agregar prisma module

--- backend/src/features/usuarios/usuarios.module.ts
+++ backend/src/features/usuarios/usuarios.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UsuariosController } from './usuarios.controller';
 import { UsuariosService } from './usuarios.service';
+import { UsuariosController } from './usuarios.controller';
+import { PrismaModule } from '../../shared/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UsuariosController],
   providers: [UsuariosService],
 })
