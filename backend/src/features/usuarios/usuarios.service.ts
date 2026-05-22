674f435d3dde540fe07f98640ccb6afd28702bb2
Miyata-Dev <hklosteverlong@gmail.com>
2026-05-22 16:50:35 -0400

    fix(modules): Cambiar el tipo de imports en los módulos

--- backend/src/features/usuarios/usuarios.service.ts
+++ backend/src/features/usuarios/usuarios.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../shared/prisma/prisma.service';
-import { Usuario } from '@prisma/client';
+import { Usuario } from '../../generated/client';
 
 @Injectable()
 export class UsuariosService {
