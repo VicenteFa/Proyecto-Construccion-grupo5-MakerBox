54b02d2a2f1003a540587770b87420aeee8cdb30
Miyata-Dev <hklosteverlong@gmail.com>
2026-06-04 09:42:44 -0400

    feat(impresiones): Agregar una funcion para cambiar el estado de impresion

--- backend/src/features/impresiones/impresiones.service.ts
+++ backend/src/features/impresiones/impresiones.service.ts
@@ -1,7 +1,25 @@
 import { Injectable } from '@nestjs/common';
-//import { PrismaService } from '../../shared/prisma/prisma.service';
+import { PrismaService } from '../../shared/prisma/prisma.service';
+import { Impresion, EstadoImpresion } from '@prisma/client';
 
 @Injectable()
 export class ImpresionesService {
-  //constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
+
+  /**
+   * Cambia el estado de una impresion especifica
+   * @param idImpresion ID (UUID) de la impresion
+   * @param estado Nuevo estado a asignar
+   * @returns La impresion actualizada
+   */
+  async cambiarEstado(idImpresion: string, estado: EstadoImpresion): Promise<Impresion> {
+    return await this.prisma.impresion.update({
+      where: {
+        idImpresion: idImpresion,
+      },
+      data: {
+        estado: estado,
+      },
+    });
+  }
 }
