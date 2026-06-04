662ebee692e8d16f87febc59bd8a9d84d08f74b4
Miyata-Dev <hklosteverlong@gmail.com>
2026-06-03 22:43:57 -0400

    feat(admin): Agregar admin page y hook de autenticación

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -4,14 +4,16 @@ import { MainLayout } from '../layouts/MainLayout';
 import { ROUTES } from '../constants/routes';
 import { HomePage } from '../pages/HomePage';
 import { SignUpPage } from '../pages/Sign-up';
+import { AdminPage } from '../pages/admin/AdminPage';
 
 export const router = createBrowserRouter([
   {
     element: <MainLayout />,
     children: [
-      { path: ROUTES.HOME, element: <HomePage /> },
-      { path: ROUTES.LOGIN, element: <LoginPage /> },
-      { path: ROUTES.SIGN_UP, element: <SignUpPage /> },
+      { path: ROUTES.HOME.path, element: <HomePage /> },
+      { path: ROUTES.LOGIN.path, element: <LoginPage /> },
+      { path: ROUTES.SIGN_UP.path, element: <SignUpPage /> },
+      { path: ROUTES.ADMIN.path, element: <AdminPage /> },
     ],
   },
 ]);
