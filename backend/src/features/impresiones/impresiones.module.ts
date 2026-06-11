317cf2e5559fd4dc71825fe78d40b66cf932217c
Vicente Farías <vicfarias23@alumnos.utalca.cl>
2026-06-11 09:55:32 -0400

    fix: arreglo con importaciones dentro del backend

--- backend/src/features/impresiones/impresiones.module.ts
+++ backend/src/features/impresiones/impresiones.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ImpresionesController } from './impresiones.controller';
 import { ImpresionesService } from './impresiones.service';
+import { PrismaModule } from '../../shared/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ImpresionesController],
   providers: [ImpresionesService],
 })
