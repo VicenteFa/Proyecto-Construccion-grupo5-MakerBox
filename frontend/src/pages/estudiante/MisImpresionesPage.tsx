d1514c56463f09e702aad5dd1e5264e80bf758b6
Vicente Farías <vicfarias23@alumnos.utalca.cl>
2026-06-14 21:14:38 -0400

    refactor: Implementar que por rol se abra una pagina correspondiente a cada rol

---
+++ frontend/src/pages/estudiante/MisImpresionesPage.tsx
@@ -0,0 +1,3 @@
+export const MisImpresionesPage = () => {
+  return <h1>Mis Impresiones</h1>;
+};
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -5,20 +5,25 @@ import { ROUTES } from '../constants/routes';
 import { HomePage } from '../pages/HomePage';
 import { SignUpPage } from '../pages/Sign-up';
 import { AdminPage } from '../pages/admin/AdminPage';
+import { SolicitudesPage } from '../pages/ayudante/SolicitudesPage';
+import { MisCursosPage } from '../pages/profesor/MisCursosPage';
+import { MisImpresionesPage } from '../pages/estudiante/MisImpresionesPage';
 
-//temporal:
+// temporal
 import { TestUsuarioPage } from '../pages/TestUsuarioPage';
 
 export const router = createBrowserRouter([
   {
     element: <MainLayout />,
     children: [
-      // prueba temporal:
       { path: '/test-usuario', element: <TestUsuarioPage /> },
       { path: ROUTES.HOME.path, element: <HomePage /> },
       { path: ROUTES.LOGIN.path, element: <LoginPage /> },
       { path: ROUTES.SIGN_UP.path, element: <SignUpPage /> },
       { path: ROUTES.ADMIN.path, element: <AdminPage /> },
+      { path: ROUTES.AYUDANTE.path, element: <SolicitudesPage /> },
+      { path: ROUTES.PROFESOR.path, element: <MisCursosPage /> },
+      { path: ROUTES.ESTUDIANTE.path, element: <MisImpresionesPage /> },
     ],
   },
 ]);
