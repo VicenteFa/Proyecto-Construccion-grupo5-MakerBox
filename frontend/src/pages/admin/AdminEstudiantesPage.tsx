4d38643e092667e4401c423adba32c170a2c3656
Paul Montecinos <pmontecinos23@alumnos.utalca.cl>
2026-07-02 23:46:09 -0400

    Agregue una vista simple sin ninguna funcionalidad en especifico

---
+++ frontend/src/pages/admin/AdminEstudiantesPage.tsx
@@ -0,0 +1,8 @@
+export const AdminEstudiantesPage = () => {
+  return (
+    <div>
+      <h1>Admin Estudiantes</h1>
+      <p>Esta es la página de administración de estudiantes.</p>
+    </div>
+  );
+};
